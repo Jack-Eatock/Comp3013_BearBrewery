9df4f0dbf12591ea01fb8457bd23fec8920e1392
BenBR <benrichards6431@hotmail.com>
2023-11-28 10:38:06 +0000

    Days
    + Added UI for what day of the week

--- Assets/Scripts/Ui/UserInterface.cs
+++ Assets/Scripts/Ui/UserInterface.cs
@@ -9,9 +9,13 @@ public class UserInterface : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI revText;
     [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI dayText;
 
-    private float timeCounter = 0f;
+    [SerializeField] private int speedOf10Minutes = 4;
+
     private int currentTimeInMinutes = 480;
+    private string[] daysOfWeek = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    private int currentDayIndex = 0;
 
     private void Awake()
     {
@@ -21,6 +25,7 @@ public class UserInterface : MonoBehaviour
             Instance = this;
 
         UpdateRevText("0");
+        UpdateDayText();
         StartCoroutine(UpdateTime());
     }
 
@@ -31,11 +36,17 @@ public class UserInterface : MonoBehaviour
 
     private IEnumerator UpdateTime()
     {
-        while (currentTimeInMinutes < 1080) // 6 PM in minutes (18 * 60)
+        while (true) // Infinite loop to keep updating time
         {
             UpdateTimeText();
-            yield return new WaitForSeconds(4); // Wait for 4 seconds
+            yield return new WaitForSeconds(speedOf10Minutes); // Wait for 4 seconds
             currentTimeInMinutes += 10; // Increment time by 10 minutes
+
+            if (currentTimeInMinutes >= 1080) // 6 PM in minutes
+            {
+                currentTimeInMinutes = 480; // Reset to 8 AM
+                UpdateDay(); // Update to the next day
+            }
         }
     }
 
@@ -44,5 +55,16 @@ public class UserInterface : MonoBehaviour
         int hours = currentTimeInMinutes / 60;
         int minutes = currentTimeInMinutes % 60;
         timeText.text = string.Format("{0:00}:{1:00}", hours, minutes); // Format time as HH:MM
+    }
+
+    private void UpdateDay()
+    {
+        currentDayIndex = (currentDayIndex + 1) % daysOfWeek.Length; // Cycle through the days
+        UpdateDayText();
+    }
+
+    private void UpdateDayText()
+    {
+        dayText.text = daysOfWeek[currentDayIndex];
     }
 }
