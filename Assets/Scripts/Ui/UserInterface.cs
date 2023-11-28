95401cf62171ef825118e38e99b402ffe383c994
BenBR <benrichards6431@hotmail.com>
2023-11-28 10:15:01 +0000

    Time UI
    + Time UI in top right
    + time passes 10mins per 4 seconds

--- Assets/Scripts/Ui/UserInterface.cs
+++ Assets/Scripts/Ui/UserInterface.cs
@@ -8,6 +8,10 @@ public class UserInterface : MonoBehaviour
     public static UserInterface Instance;
 
     [SerializeField] private TextMeshProUGUI revText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private float timeCounter = 0f;
+    private int currentTimeInMinutes = 480;
 
     private void Awake()
     {
@@ -17,10 +21,28 @@ public class UserInterface : MonoBehaviour
             Instance = this;
 
         UpdateRevText("0");
+        StartCoroutine(UpdateTime());
     }
 
     public void UpdateRevText(string text)
     {
         revText.text = "Revenue: £" + text;
+    }
+
+    private IEnumerator UpdateTime()
+    {
+        while (currentTimeInMinutes < 1080) // 6 PM in minutes (18 * 60)
+        {
+            UpdateTimeText();
+            yield return new WaitForSeconds(4); // Wait for 4 seconds
+            currentTimeInMinutes += 10; // Increment time by 10 minutes
+        }
+    }
+
+    private void UpdateTimeText()
+    {
+        int hours = currentTimeInMinutes / 60;
+        int minutes = currentTimeInMinutes % 60;
+        timeText.text = string.Format("{0:00}:{1:00}", hours, minutes); // Format time as HH:MM
     }
 }
