0a027b3cad2d8f86474df821312ab33d27210cef
Jack Eatock <jack.eatock06@gmail.com>
2023-12-08 17:52:42 +0000

    Reworked machines

--- Assets/Scripts/Components/Buildings/Combiner.cs
+++ Assets/Scripts/Components/Buildings/Combiner.cs
@@ -5,194 +5,52 @@ using UnityEngine;
 
 namespace DistilledGames
 {
-    public class Combiner : Building, IInteractable, IConveyerInteractable
+    public class Combiner : BaseMachine
     {
-        [SerializeField] private List<Recipe> recipes; // This will hold the references to the Recipe ScriptableObjects
-        [SerializeField] private int maxCapacity;
-        [SerializeField] private float processingTime;
+        [SerializeReference]
+        private AudioClip runningSound, outputSound, inputSound;
+        private SFXInGame sfxController;
 
-        private Dictionary<int, List<Recipe>> recipeDictionary; // Maps an item ID to its recipes
-        private Dictionary<int, int> inputCounts; // Tracks the counts of each input item ID
-        private int outputItemCount = 0;
-        private bool isProcessing = false;
-        private List<Recipe.ItemCountPair> currentOutputItems;
 
-        [SerializeField]
-        private List<Vector2Int> conveyerIn, conveyerOut;
-
-        private void Start()
+        protected override void Awake()
         {
-            recipeDictionary = new Dictionary<int, List<Recipe>>();
-            inputCounts = new Dictionary<int, int>();
-
-            // Iterate over each recipe to populate our dictionary
-            foreach (Recipe recipe in recipes)
-            {
-                foreach (var inputItem in recipe.InputItems)
-                {
-                    if (!recipeDictionary.ContainsKey(inputItem.itemPrefab.ItemID))
-                    {
-                        recipeDictionary[inputItem.itemPrefab.ItemID] = new List<Recipe>();
-                    }
-                    recipeDictionary[inputItem.itemPrefab.ItemID].Add(recipe);
-
-                    // Initialize inputCounts dictionary
-                    inputCounts[inputItem.itemPrefab.ItemID] = 0;
-                }
-            }
+            base.Awake();
+            sfxController = GetComponent<SFXInGame>();
         }
 
-        void Update()
+        protected override void Start()
         {
-            // Process items if not currently processing and there is a recipe that can be made
-            if (!isProcessing && CanProcessAnyRecipe())
-            {
-                StartCoroutine(ProcessItems());
-            }
+            base.Start();
         }
 
-        private bool CanProcessAnyRecipe() // Is there a recipe that can be currently processed?
+        protected override void Update()
         {
-            foreach (var recipe in recipes)
-            {
-                bool canMake = true;
-                foreach (var inputItem in recipe.InputItems)
-                {
-                    if (inputCounts[inputItem.itemPrefab.ItemID] < inputItem.itemCount)
-                    {
-                        canMake = false;
-                        break;
-                    }
-                }
-
-                if (canMake)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            base.Update();
         }
 
-        private IEnumerator ProcessItems()
+        protected override void UpdateSprite()
         {
-            isProcessing = true;
-            yield return new WaitForSeconds(processingTime);
+            base.UpdateSprite();
 
-            // Find and process a valid recipe
-            foreach (var recipe in recipes)
-            {
-                bool canProcess = true;
-                foreach (var input in recipe.InputItems)
-                {
-                    if (inputCounts[input.itemPrefab.ItemID] < input.itemCount)
-                    {
-                        canProcess = false;
-                        break;
-                    }
-                }
+            // Check if there are items in the input or output 
+            bool isActive = CanWeProcess();
 
-                if (canProcess)
-                {
-                    // Consume the input items
-                    foreach (var input in recipe.InputItems)
-                    {
-                        inputCounts[input.itemPrefab.ItemID] -= input.itemCount;
-                    }
-
-                    // Store outputs for retrieval
-                    currentOutputItems = new List<Recipe.ItemCountPair>(recipe.OutputItems);
-                    outputItemCount += recipe.OutputItems.Count;
-                    break;
-                }
-            }
-
-            isProcessing = false;
-        }
-
-        #region Player Interaction
-
-        public bool TryToInsertItem(Item item)
-        {
-            if (recipeDictionary.TryGetValue(item.ItemID, out List<Recipe> possibleRecipes))
-            {
-                if (inputCounts[item.ItemID] < maxCapacity)
-                {
-                    inputCounts[item.ItemID]++;
-                    Destroy(item.gameObject); // Consuming the item
-                    Debug.Log("Item added to the combiner.");
-                    return true;
-                }
-            }
-
-            Debug.Log("Cannot add item to combiner. It may be full or not suitable for any recipe.");
-            return false;
-        }
-
-        public bool TryToRetreiveItem(out Item item)
-        {
-            item = null;
-            if (currentOutputItems != null && currentOutputItems.Count > 0)
-            {
-                var outputPair = currentOutputItems[0];
-                currentOutputItems.RemoveAt(0); // Remove the item from the list
-                item = Instantiate(outputPair.itemPrefab); // Instantiate the output item
-                item.transform.position = transform.position;
-                item.SetInteractable(false);
-                Debug.Log("Item retrieved from combiner. Items left: " + outputItemCount);
-                outputItemCount--;
-                return true;
-            }
+            if (isActive)
+                sfxController.LoopingClipPlay(runningSound, 1f);
             else
-            {
-                Debug.Log("No items to retrieve from combiner.");
-                return false;
-            }
+                sfxController.LoopingClipStop();
         }
-        #endregion
 
-        #region Conveyers
-
-        public bool ConveyerTryToInsertItem(Item item, Vector2Int insertFromCoords)
+        protected override void InputtedFromConveyer()
         {
-            return TryToInsertItem(item);
+            base.InputtedFromConveyer();
+            sfxController.PlayOneClip(inputSound, 1f);
         }
 
-        public bool ConveyerTryToRetrieveItem(Vector2Int RetrieveFromCoords, out Item item)
+        protected override void OutputedToConveyer()
         {
-            item = null;
-            Vector2Int gridCoordsAdjusted = Vector2Int.zero;
-            for (int i = 0; i < conveyerOut.Count; i++)
-            {
-                gridCoordsAdjusted = GridCoords + conveyerOut[i];
-                if (RetrieveFromCoords == gridCoordsAdjusted)
-                    return TryToRetreiveItem(out item);
-            }
-            return false;
+            base.OutputedToConveyer();
+            sfxController.PlayOneClip(outputSound, 1f);
         }
-
-        public bool CanAnItemBeInserted(Item item, Vector2Int insertFromCoords)
-        {
-            if (recipeDictionary.TryGetValue(item.ItemID, out List<Recipe> possibleRecipes))
-            {
-                if (inputCounts[item.ItemID] < maxCapacity)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool CanConnectIn(Vector2Int coords)
-        {
-            Vector2Int gridCoordsAdjusted = Vector2Int.zero;
-            for (int i = 0; i < conveyerIn.Count; i++)
-            {
-                gridCoordsAdjusted = GridCoords + conveyerIn[i];
-                if (coords == gridCoordsAdjusted)
-                    return true;
-            }
-            return false;
-        }
-
-        #endregion
     }
 }
